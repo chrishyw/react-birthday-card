618a73e2d660d3dc458b884322f9e0141385b73f
Christopher Wang <hycwang@ucdavis.edu>
2018-11-12 03:11:23 -0800

    Updates

---
+++ precache-manifest.6db195bdcbc02762a8dfce280f3e24b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4542335dd5359d94bd7d8314a89edcaa",
+    "url": "/react-birthday-card/static/media/omg.4542335d.jpg"
+  },
+  {
+    "revision": "d891f7f137eeeb18fab1b780bea7478a",
+    "url": "/react-birthday-card/static/media/S__42827780.d891f7f1.jpg"
+  },
+  {
+    "revision": "3ca9e7d05f972f94bd9a",
+    "url": "/react-birthday-card/static/js/runtime~main.3ca9e7d0.js"
+  },
+  {
+    "revision": "288b279eed3ce6d69822",
+    "url": "/react-birthday-card/static/js/main.288b279e.chunk.js"
+  },
+  {
+    "revision": "32a48f9a6e0f954a9845",
+    "url": "/react-birthday-card/static/js/1.32a48f9a.chunk.js"
+  },
+  {
+    "revision": "288b279eed3ce6d69822",
+    "url": "/react-birthday-card/static/css/main.fe12e041.chunk.css"
+  },
+  {
+    "revision": "32a48f9a6e0f954a9845",
+    "url": "/react-birthday-card/static/css/1.a84b948a.chunk.css"
+  },
+  {
+    "revision": "6b35d8db3a1fd9186c49661e6cac5ef8",
+    "url": "/react-birthday-card/index.html"
+  }
+];
